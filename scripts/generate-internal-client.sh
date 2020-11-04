81e1659330a359ab4b5eb1b70ff26eac3b38b05a
Seth Silesky <seth.silesky@gmail.com>
2020-11-04 16:33:58 -0600

    add generated code

--- scripts/generate-internal-client.sh
+++ scripts/generate-internal-client.sh
@@ -2,4 +2,5 @@
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
   -i https://github.com/ShipEngine/shipengine-openapi/blob/master/openapi.json?raw=true \
-  --skip-validate-spec -g typescript-fetch -o /local/src/services-internal/shipengine-generated --additional-properties=npmName=ShipengineInternalClient,supportsES6=true
+  --skip-validate-spec -g typescript-fetch -o /local/src/services-internal/shipengine-generated \
+  --additional-properties=npmName=ShipengineInternalClient,supportsES6=true
