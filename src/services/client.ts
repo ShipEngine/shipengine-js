a7a275963bf8d374d9b9abf1203263e4538095bc
Seth Silesky <seth.silesky@gmail.com>
2020-11-04 14:24:48 -0600

    add hoverfloy

---
+++ src/services/client.ts
@@ -0,0 +1,42 @@
+import fetch from "isomorphic-unfetch";
+const BASE_URL = "http://foo.com";
+
+type ClientOptions = {
+  data?: any;
+} & RequestInit;
+
+/**
+ * Make fetch more like axios
+ */
+const Client = <Data = any>(
+  baseUrl: string,
+  baseConfig: ClientOptions = {}
+) => (
+  endpoint: string,
+  { data, ...customConfig }: ClientOptions = {}
+): Promise<Data> => {
+  const config: RequestInit = {
+    ...baseConfig,
+    ...customConfig,
+    headers: {
+      "Content-Type": "application/json",
+      ...baseConfig.headers,
+      ...customConfig.headers,
+    },
+  };
+  if (data) {
+    config.body = JSON.stringify(data);
+  }
+  return fetch(`${baseUrl}/${endpoint}`, config).then(async (response) => {
+    const data = await response.json();
+    // assumes that this will always return json.
+    if (response.ok) {
+      return data;
+    }
+    return Promise.reject(data);
+  });
+};
+
+const client = Client(BASE_URL);
+
+export { client };
