a3676dc4efaaf607f62d3407503c61041ee5138f
Seth Silesky <seth.silesky@gmail.com>
2020-11-18 14:54:07 -0600

    works

--- src/services/tags.ts
+++ src/services/tags.ts
@@ -5,20 +5,36 @@ export interface ITagsService {
   create(tagName: string): Promise<string>;
 }
 
+export type AdvancedAPI = ReturnType<TagsService['getAdvancedAPI']>;
+export type ConvenienceAPI = ReturnType<TagsService['getConvenienceAPI']>;
 /**
  * Publicly-accessible service for anything related to tags
  */
-export class TagsService implements ITagsService {
+export class TagsService {
   private client: AxiosInstance;
   constructor(client: TagsService['client']) {
     this.client = client;
   }
-  public async get() {
+  public getConvenienceAPI() {
+    return {
+      createTag: this.createTag,
+    };
+  }
+  public getAdvancedAPI() {
+    return {
+      tags: {
+        get: this.get,
+        create: this.create,
+      },
+    };
+  }
+  private async get() {
     const { data } = await this.client.get<string[]>('/tags');
     return data;
   }
-  public async create(tagName: string) {
+  private async create(tagName: string) {
     const { data } = await this.client.post<string>(`/tags/${tagName}`, {});
     return data;
   }
+  private createTag = this.create;
 }
