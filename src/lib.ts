68dd66c6006c2dc16d0a4a50426f5fe22db698bf
Seth Silesky <seth.silesky@gmail.com>
2020-11-18 17:32:14 -0600

    make service factory a factory fn

--- src/lib.ts
+++ src/lib.ts
@@ -3,4 +3,4 @@ import { ShipEngineApi } from './services/shipengine-api-factory';
 
 // should this api key be optional here in case of users who want to _only_ use api keys on a request by request basis?
 export const ShipEngine = (apiKey: string) =>
-  new ServiceFactory(ShipEngineApi({ apiKey }));
+  ServiceFactory(ShipEngineApi({ apiKey }));
--- src/services/service-factory.ts
+++ src/services/service-factory.ts
@@ -2,43 +2,28 @@ import type { ShipEngineApiClient } from './shipengine-api-factory';
 import { TagsService } from './tags';
 import { AddressService } from './address';
 
-class _ServiceFactory {
-  public constructor(client: ShipEngineApiClient) {
-    // simple multi-object inheritance strategy -- unfortunately, not type safe.
-    const tags = new TagsService(client);
-    Object.assign(this, tags.getAdvancedAPI(), tags.getConvenienceAPI());
-
-    const address = new AddressService(client);
-    Object.assign(this, address.getAdvancedAPI(), address.getConvenienceAPI());
-  }
-}
-
-type InferPublicFields<T> = T extends {
+type GetPublicAPI<T> = T extends {
   getAdvancedAPI: (...args: any[]) => infer AdvancedAPIFields;
   getConvenienceAPI: (...args: any[]) => infer ConvenienceAPIAPIFields;
 }
   ? AdvancedAPIFields & ConvenienceAPIAPIFields
   : never;
 
-type PublicFields = InferPublicFields<TagsService> &
-  InferPublicFields<AddressService>;
-
-// https://github.com/microsoft/TypeScript/issues/26792
-export const ServiceFactory = _ServiceFactory as {
-  new (client: any): _ServiceFactory & PublicFields;
-};
+type PublicServiceAPI = GetPublicAPI<TagsService> &
+  GetPublicAPI<AddressService>;
 
 // here is another way of doing the exact same thing without a service
-export const createServices = (client: ShipEngineApiClient) => {
+export const ServiceFactory = (client: ShipEngineApiClient) => {
   const services = [TagsService, AddressService];
-  const mapped = services.map((EachService) => {
-    const f = new EachService(client);
-    return {
-      ...f.getAdvancedAPI(),
-      ...f.getConvenienceAPI(),
-    };
-  });
 
-  const result = Object.assign({}, ...mapped) as PublicFields;
-  return result;
+  const publicServices = services.reduce((acc, EachService) => {
+    const service = new EachService(client);
+    return {
+      ...acc,
+      ...service.getAdvancedAPI(),
+      ...service.getConvenienceAPI(),
+    };
+  }, {} as PublicServiceAPI);
+
+  return publicServices;
 };
