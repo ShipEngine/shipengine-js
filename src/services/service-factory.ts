f338b7352f360eb3d5b72d6bdf64edb0eb92a0f1
Seth Silesky <seth.silesky@gmail.com>
2020-11-18 14:56:23 -0600

    works

--- src/services/service-factory.ts
+++ src/services/service-factory.ts
@@ -3,6 +3,7 @@ import { TagsService } from './tags';
 
 export class ServiceFactory {
   public constructor(client: ShipEngineApiClient) {
+    // simple multi-object inheritance strategy -- unfortunately, not type safe.
     const tags = new TagsService(client);
     Object.assign(this, tags.getAdvancedAPI(), tags.getConvenienceAPI());
   }
