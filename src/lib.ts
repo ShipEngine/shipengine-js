a15171958cc5f3063d39e4a0a67da6b40a50d788
Seth Silesky <seth.silesky@gmail.com>
2020-11-20 15:55:32 -0600

    add

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,28 @@
+import { ShipEngineApiServiceConfig } from './models/Config';
 import { ServiceFactory } from './services/service-factory';
 import { ShipEngineApi } from './services/shipengine-api-factory';
 
 // should this api key be optional here in case of users who want to _only_ use api keys on a request by request basis?
-export const ShipEngine = (apiKey: string) =>
-  ServiceFactory(ShipEngineApi({ apiKey }));
+
+type ApiKey = string;
+
+type SimplifiedConfig = {
+  apiKey: ApiKey;
+  baseUrl?: string;
+};
+
+type ShipEngineConfig = ApiKey | SimplifiedConfig;
+
+const mapToApiServiceConfig = (
+  config: ShipEngineConfig
+): ShipEngineApiServiceConfig => {
+  if (typeof config === 'string') {
+    return { apiKey: config };
+  }
+
+  return { apiKey: config.apiKey, requestOptions: { baseURL: config.baseUrl } };
+};
+
+export const ShipEngine = (config: ShipEngineConfig) => {
+  return ServiceFactory(ShipEngineApi(mapToApiServiceConfig(config)));
+};
