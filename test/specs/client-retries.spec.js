c4dbd7e25182d3c9c4b6d46d52e1e61cbfa25e46
Seth Silesky <seth.silesky@gmail.com>
2020-11-24 11:45:24 -0600

    works

--- test/specs/client-retries.spec.js
+++ test/specs/client-retries.spec.js
@@ -1,14 +1,20 @@
-const { ShipEngine } = require('../../cjs/index');
+const {
+  ShipEngineApiClient,
+} = require('../../cjs/services/shipengine-api-factory');
 const { Hoverfly } = require('../utils/Hoverfly');
 const { expect } = require('chai');
 
-describe.only('retries', () => {
+describe('retries', () => {
+  const get = () =>
+    ShipEngineApiClient({
+      apiKey: '123',
+      requestOptions: {
+        baseURL: 'http://localhost:8500',
+      },
+    }).get('/retries');
+
   before(async () => {
     await Hoverfly.start();
-    api = ShipEngine({
-      apiKey: 'myApiKey',
-      baseUrl: 'http://localhost:8500/v1',
-    });
   });
   after(async () => {
     await Hoverfly.stop();
@@ -16,18 +22,18 @@ describe.only('retries', () => {
 
   it('should try again if 429 error', async () => {
     await Hoverfly.import('client/retry/429.json');
-    const res = await api.createTag('foo');
-    expect(res.name).to.eq('foo');
+    const { status } = await get();
+    expect(status).to.eq(200);
   });
   it('should try again if 500 error', async () => {
     await Hoverfly.import('client/retry/500.json');
-    const res = await api.createTag('foo');
-    expect(res.name).to.eq('foo');
+    const { status } = await get();
+    expect(status).to.eq(200);
   });
   it('should not try again if 404 error', async () => {
     await Hoverfly.import('client/retry/404.json');
     try {
-      await api.createTag('foo');
+      await get();
       expect(false).to.be.true; // should throw error
     } catch (err) {
       expect(err.response.status).to.eq(404);
