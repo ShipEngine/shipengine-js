a308ffb2a20c5b068f7869940661980798609b16
Seth Silesky <seth.silesky@gmail.com>
2020-12-03 11:48:20 -0600

    add util test

--- src/models/api/index.ts
+++ src/models/api/index.ts
@@ -12,12 +12,16 @@ import type {
   ResponseMessage as _ResponseMessage,
 } from './validate-address/validate_address_response_body';
 
-export type AddressValidationResult = Overwrite<
-  _AddressValidationResult,
-  { matched_address: _AddressValidationResult['matched_address'] | null }
->;
-
 export type ResponseMessage = _ResponseMessage;
 
 export type ValidateAddressResponseBody = AddressValidationResult[];
+
 export type MatchedAddress = _PartialAddress1 | _PartialAddress;
+
+/**
+ * OpenAPI Definition is wrong -- matched_address will come back null
+ */
+export type AddressValidationResult = Overwrite<
+  _AddressValidationResult,
+  { matched_address: MatchedAddress | null }
+>;
