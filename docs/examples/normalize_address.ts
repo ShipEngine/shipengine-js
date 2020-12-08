063e7f42af9b031c77806d432aeec752288259b5
Seth Silesky <seth.silesky@gmail.com>
2020-12-08 15:05:20 -0600

    add readme

---
+++ docs/examples/normalize_address.ts
@@ -0,0 +1,42 @@
+/* normalize_address.ts */
+/* initialize */
+
+/* import ShipEngine from 'shipengine'; */
+/* warning: below is an internal import for example purposes, use module import ^ in your code */
+import ShipEngine from '../../src';
+
+const shipengine = ShipEngine('my_api_key');
+
+/* normalize address args */
+shipengine
+  .normalizeAddress({
+    street: ['1 E 161 St'],
+    country: 'US',
+    cityLocality: 'The Bronx',
+    stateProvince: 'NY',
+  })
+  .then((address) => {
+    console.log(`normalized street is: ${address.street}`);
+    console.log(`is residential: ${address.isResidential}`);
+  });
+
+/* exception handling */
+shipengine
+  .normalizeAddress({ street: '1234 Main St' })
+  .catch((err) => console.error('exception!'));
+
+/* normalize address service */
+shipengine.addresses
+  .normalize([
+    { street: '1234 Main St' },
+    {
+      street: ['1 E 161 St'],
+      country: 'US',
+      cityLocality: 'The Bronx',
+      stateProvince: 'NY',
+    },
+  ])
+  .then(([addr1, addr2]) => {
+    console.assert(addr1 === undefined, 'first address cannot be normalized');
+    console.assert(!!addr2, 'second address should be valid');
+  });
---
+++ docs/examples/validate_address.ts
@@ -0,0 +1,41 @@
+/* validate_address.ts */
+/* initialize */
+
+/* import ShipEngine from 'shipengine'; */
+/* warning: below is an internal import for example purposes, use module import ^ in your code */
+import ShipEngine from '../../src';
+
+const shipengine = ShipEngine('my_api_key');
+
+/* validate address args */
+shipengine
+  .validateAddress({
+    street: ['1 E 161 St'],
+    country: 'US',
+    cityLocality: 'The Bronx',
+    postalCode: '10451',
+    stateProvince: 'NY',
+  })
+  .then((isValid) => console.log(isValid ? 'valid!' : 'invalid!'));
+
+/* validate address service */
+shipengine.addresses
+  .validate([
+    {
+      street: ['1 E 161 St'],
+      country: 'US',
+      cityLocality: 'The Bronx',
+      postalCode: '10451',
+      stateProvince: 'NY',
+    },
+    {
+      street: ['4009 Marathon Blvd', 'Suite 200'],
+      country: 'US',
+      cityLocality: 'The Bronx',
+      stateProvince: 'TX',
+      country: 'US',
+    },
+  ])
+  .then(([isValid1, isValid2]) => {
+    console.log(isValid1 && isValid2 ? 'all are valid' : 'some are invalid');
+  });
