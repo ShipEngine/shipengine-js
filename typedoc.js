3a3f07b27e2fbd7dc951abf65fed344018aedb0e
Seth Silesky <seth.silesky@gmail.com>
2020-12-09 15:44:20 -0600

    update README

--- typedoc.js
+++ typedoc.js
@@ -1,5 +1,7 @@
+// https://typedoc.org/guides/options
 module.exports = {
   inputFiles: ['./src'],
   mode: 'modules',
+  disableSources: true, // blob changes all the time, so disable
   out: 'docs/api',
 };
