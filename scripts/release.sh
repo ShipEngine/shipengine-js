891fe0d64c8d3a192c56934a884400890760c587
Seth Silesky <seth.silesky@gmail.com>
2020-12-13 16:44:38 -0600

    add version bump

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "main" ]]; then
+  echo '--> Only execute this on main branch'
+  exit 0
+fi
+
+# don't create git tag here, it will automatically be created in the next step
+npx bump prerelease --preid alpha --push
+
+# github will automatically create a tag when you create a release
+tag="v$(node -p "require('./package.json').version")"
+gh release create \
+  "$tag" \
+  --prerelease \
+  --draft \
+  --title "$tag" \
+  --notes "releases $tag"
