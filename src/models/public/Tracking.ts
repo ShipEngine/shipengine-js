bc2a91cc99fe4a55e7f6af57f3a58f17267e76df
Seth Silesky <seth.silesky@gmail.com>
2020-12-16 17:56:12 -0600

    add tracking data types

--- src/models/public/Tracking.ts
+++ src/models/public/Tracking.ts
@@ -1,5 +1,5 @@
-import { isCompletelyNullOrEmptyObject } from '../../utils';
 import { ISOString } from './DateTime';
+import { ShipEngineException } from './ShipEngineException';
 
 /**
  * Shipment Statuses
@@ -16,45 +16,96 @@ export enum TrackingStatus {
 }
 
 /**
- * types are not nested (e.g. TrackingEvent versus Tracking.Event)
+ * A location
  */
 interface TrackingEventLocation {
+  /* City or locality. */
   cityLocality?: string;
+
+  /* State or province. */
   stateProvince?: string;
+
+  /* Postal code. */
   postalCode?: string;
+
+  /* [ISO 3166](https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes) country code. */
   countryCode?: string;
+
+  /* Latitude. */
   latitude?: number;
+
+  /* Longitude. */
   longitude?: number;
 }
-
-export class TrackingEvent {
+/* An event or status change that occurred while processing a `Shipment`. */
+export interface TrackingEvent {
+  /* Date, datetime, or datetime w/timezone at which the event occurred. */
   dateTime: ISOString;
+
+  /* Status of the shipment. */
   status: TrackingStatus;
+
+  /* Long-form description of the status */
   description: string;
+
+  /*  Carrier-specific event or status code. */
   carrierStatusCode: string;
+
+  /* Carrier-specific description */
   carrierDetailCode: string;
+
+  /* Location where the event occurred. */
   location?: TrackingEventLocation;
+
+  /* Human-readable information regarding this event, such as details about the error state
+   * or a description of where the package was placed upon delivery.
+   */
   notes: string[];
+
+  /*  Name of the person who signed or approved this event.
+   * This is usually only relevant for the TrackingStatus.Delivered event.
+   */
   signer?: string;
-  constructor(
-    dateTime: ISOString,
-    status: TrackingStatus,
-    description: string,
-    carrierStatusCode: string,
-    carrierDetailCode: string,
-    notes: string[],
-    location: TrackingEventLocation,
-    signer?: string
-  ) {
-    this.dateTime = dateTime;
-    this.status = status;
-    this.description = description;
-    this.carrierStatusCode = carrierStatusCode;
-    this.carrierDetailCode = carrierDetailCode;
-    if (location && !isCompletelyNullOrEmptyObject(location)) {
-      this.location = location;
-    }
-    this.notes = notes;
-    this.signer = signer;
-  }
+}
+
+export interface TrackingInformation {
+  /* Carrier code for the shipment. */
+  carrierCode: string;
+
+  /* Tracking number for the shipment. */
+  trackingNumber: string;
+
+  /* Date, datetime, datetime w/timestamp estimated delivery. */
+  estimatedDelivery: ISOString;
+
+  /* `Event`s that have occurred for this shipment. */
+  events: TrackingEvent[];
+
+  /*  Returns the latest `Event`. */
+  readonly latestEvent?: TrackEvent;
+
+  /*  The datetime of the first "accepted" event in the `events` array, if any. */
+  readonly shippedAt?: ISOString;
+
+  /* The datetime of the last "delivered" event in the `events` array, if any. */
+  readonly deliveredAt?: ISOString;
+}
+
+export interface TrackingQuery {
+  /* Tracking number of the shipment being queried. */
+  trackingNumber: string;
+  /* Carrier code of the shipment being queried. */
+  carrierCode: string;
+}
+
+/* Result of a tracking query. */
+export interface TrackingQueryResult {
+  /* Either a `Query` or `String` representing a label_id. */
+  query: TrackingQuery | string;
+
+  /* Information. */
+  information: TrackingInformation;
+
+  /* Exceptions */
+  exceptions: ShipEngineException[];
 }
--- src/models/public/index.ts
+++ src/models/public/index.ts
@@ -3,3 +3,4 @@ export * from './Config';
 export * from './ShipEngineException';
 export * from './Services';
 export * from './DateTime';
+export * from './Tracking';
