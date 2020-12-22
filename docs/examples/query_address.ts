9dc5ec9bcc75ddf9dd6d15654bfb571fba969464
Seth Silesky <seth.silesky@gmail.com>
2020-12-22 14:03:28 -0600

    update docs

--- docs/examples/query_address.ts
+++ docs/examples/query_address.ts
@@ -5,21 +5,25 @@ import { default as ShipEngine } from '../../src';
 import 'dotenv/config';
 const shipengine = new ShipEngine(process.env.API_KEY);
 
+
 /* wrapper start */
 (async () => {
-  /* query address */
-  const addressQuery = await shipengine.queryAddress({
-    street: ['1 E 161 St'],
-    cityLocality: 'The Bronx',
-    stateProvince: 'NY',
-    postalCode: '10451',
-    country: 'US',
-  });
 
-  console.assert(!addressQuery.exceptions.length, 'should be no exceptions');
-  console.log(
-    `the normalized address is: ${JSON.stringify(addressQuery.normalized)}.`
-  );
 
-  /* wrapper end */
-})();
+/* query address */
+const addressQuery = await shipengine.queryAddress({
+  street: ['1 E 161 St'],
+  cityLocality: 'The Bronx',
+  stateProvince: 'NY',
+  postalCode: '10451',
+  country: 'US'
+})
+
+console.assert(!addressQuery.exceptions.length, 'should be no exceptions')
+console.log(`the normalized address is: ${JSON.stringify(addressQuery.normalized)}.`)
+
+
+/* wrapper end */
+})()
+
+
