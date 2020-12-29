3886cc410ba585f4baa6a5890f6454667443d26b
Seth Silesky <seth.silesky@gmail.com>
2020-12-29 14:02:59 -0600

    update tracking examples

--- docs/examples/track-shipment.ts
+++ docs/examples/track-shipment.ts
@@ -9,39 +9,39 @@ const shipengine = new ShipEngine(process.env.API_KEY);
 (async () => {
   /* one */
   try {
-    const { estimatedDelivery, events } = await shipengine.trackShipment({
-      trackingNumber: '1Z97643X0347662293',
+    const result = await shipengine.trackShipment({
+      trackingNumber: '1Z12345E0205271688',
       carrierCode: 'ups',
     });
-
     console.assert(
-      estimatedDelivery !== undefined,
+      result.estimatedDelivery,
       'should have an estimated delivery'
     );
-    console.assert(events.length, 'should have tracking events');
+    console.assert(result.events.length, 'should have tracking events');
   } catch (err) {
     console.error(err);
   }
 
   /* two */
+  /*
   try {
     const result = await shipengine.trackShipment('se-ABC123');
-    console.assert(result !== undefined);
+    console.assert(
+      result.estimatedDelivery,
+      'should have an estimated delivery'
+    );
+    console.assert(result.events.length, 'should have tracking events');
   } catch (err) {
     console.error(err);
   }
-
+*/
   /* three */
-  try {
+  /*   try {
     await shipengine.trackShipment('se-IDONTEXIST');
-    console.error('should throw an error; execution should not reach here');
   } catch (err) {
-    console.assert(err.type === 'error');
-    console.assert(
-      err.message !== undefined,
-      'error should have message field'
-    );
-  }
+    console.error(err);
+    console.assert(err.message, 'should have message field');
+  } */
 
   /* wrapper end */
 })();
