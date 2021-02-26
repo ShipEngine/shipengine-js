3ec47a0b95f59c1c3c520f09be87952b4ab21b51
GitHub Action <action@github.com>
2021-02-26 16:21:21 +0000

    generate docs [skip ci]

--- docs/examples/create-tags.ts
+++ docs/examples/create-tags.ts
@@ -8,11 +8,11 @@ const shipengine = new ShipEngine(process.env.API_KEY);
 /* wrapper start */
 (async () => {
   /* either */
-  const response = await shipengine.createTag('MY_TAG');
+  const response = await shipengine.tags.create('MY_TAG');
   if (response.type === 'success') {
-    console.log('Tag created!', response.result);
+    console.log('Tag created!', response.result.name);
   } else {
-    console.error('error', response.error);
+    console.error('error', response.error.message);
   }
 
   console.assert(response.type !== 'error', 'Tag should be successful');
