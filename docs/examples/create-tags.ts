3476341d2a1458df3b4ffbeb845c059fc3b7610e
GitHub Action <action@github.com>
2021-02-26 03:23:34 +0000

    generate docs [skip ci]

---
+++ docs/examples/create-tags.ts
@@ -0,0 +1,11 @@
+/* create-tags.ts */
+/* initialize */
+import { default as ShipEngine } from '../../src';
+
+import 'dotenv/config';
+const shipengine = new ShipEngine(process.env.API_KEY);
+
+/* wrapper start */
+(async () => {
+  /* wrapper end */
+})();
