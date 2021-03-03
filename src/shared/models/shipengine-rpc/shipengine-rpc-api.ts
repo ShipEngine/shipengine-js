5f35fc91b66ea5fab0d9717ee834866e9f5fae63
Seth Silesky <seth.silesky@gmail.com>
2021-03-03 15:45:19 -0600

    add mapper fn

--- src/shared/models/shipengine-rpc/shipengine-rpc-api.ts
+++ src/shared/models/shipengine-rpc/shipengine-rpc-api.ts
@@ -1,20 +1,16 @@
 import { InternalRpcClient } from './client';
-import { bimap, id } from '../../../utils';
-import type * as T from './types';
-import { toValidateAddressResult, toValidateAddressParamsDto } from './types';
+import * as T from './types';
+
 export class ShipEngineRpcApiClient extends InternalRpcClient {
   createTag = async (tag: T.CreateTagParams) => {
-    return this.exec<T.CreateTagParams, T.CreateTagResult>('tag/create', tag);
+    return this.exec('tag/create', tag, (v) => v);
   };
 
   validateAddress = async (validateAddressParams: T.ValidateAddressParams) => {
-    return bimap(
-      await this.exec<T.ValidateAddressParamsDto, T.ValidateAddressResultDto>(
-        'address/validate',
-        toValidateAddressParamsDto(validateAddressParams)
-      ),
-      toValidateAddressResult,
-      id
+    return this.exec(
+      'address/validate',
+      T.toValidateAddressParamsDto(validateAddressParams),
+      T.toValidateAddressResult
     );
   };
 }
