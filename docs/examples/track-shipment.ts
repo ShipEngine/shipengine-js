514561e464af54f2c2f4382558512857c2ea9fcb
GitHub Action <action@github.com>
2021-03-08 04:51:58 +0000

    generate docs [skip ci]

--- docs/examples/track-shipment.ts
+++ docs/examples/track-shipment.ts
@@ -9,20 +9,38 @@ const shipengine = new ShipEngine(process.env.API_KEY);
 (async () => {
   /* one */
   try {
-    const result = await shipengine.trackPackage({
+    const result = await shipengine.trackShipment({
       trackingNumber: '1Z12345E0205271688',
       carrierCode: 'ups',
     });
     console.assert(
-      result.information.estimatedDelivery,
+      result.estimatedDelivery,
       'should have an estimated delivery'
     );
-    console.assert(
-      result.information.events.length,
-      'should have tracking events'
-    );
+    console.assert(result.events.length, 'should have tracking events');
   } catch (err) {
     console.error(err);
   }
+
+  /* two */
+  try {
+    const result = await shipengine.trackShipment('se-ABC123');
+    console.assert(
+      result.estimatedDelivery,
+      'should have an estimated delivery'
+    );
+    console.assert(result.events.length, 'should have tracking events');
+  } catch (err) {
+    console.error(err);
+  }
+
+  /* three */
+  try {
+    await shipengine.trackShipment('se-IDONTEXIST');
+  } catch (err) {
+    console.error(err);
+    console.assert(err.message, 'should have message field');
+  }
+
   /* wrapper end */
 })();
