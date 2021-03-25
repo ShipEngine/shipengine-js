68e4047ee60ca5791149bb86a8cf93d896236346
Seth Silesky <seth.silesky@gmail.com>
2021-03-25 14:37:02 -0500

    update track package

---
+++ src/shared/models/package-status.ts
@@ -0,0 +1,8 @@
+export enum Status {
+  Accepted = 'ACCEPTED',
+  AttemptedDelivery = 'ATTEMPTED DELIVERY',
+  Delivered = 'DELIVERED',
+  Exception = 'EXCEPTION',
+  InTransit = 'IN TRANSIT',
+  Unknown = 'UNKNOWN',
+}
