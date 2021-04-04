4e493c91044ed183779e2498f6b456e53d245477
James Messinger <james.messinger@gmail.com>
2021-04-04 10:58:36 -0500

    Removed unneeded files Removed VSCode config from source control

--- scripts/generate-internal-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-  -i https://github.com/ShipEngine/shipengine-openapi/blob/master/openapi.yaml?raw=true \
-  --skip-validate-spec --generator-name typescript-fetch -o /local/src/services-internal/generated
