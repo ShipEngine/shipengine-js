23ab302a3503e2b1da15e441615fcc49ed49b51e
devmandy <mandy.hubbard@shipengine.com>
2021-04-06 16:20:29 -0500

    Refactored from class with constructor to POJO with method

--- src/address/normalized-address.ts
+++ src/address/normalized-address.ts
@@ -4,83 +4,94 @@ import { NormalizedAddressDTO } from "../json-rpc";
 /**
  * A normalized address that is returned by ShipEngine API.
  */
-export class NormalizedAddress {
+export interface NormalizedAddress {
   /**
    * The street address. Each string in the array is a separate line (up to 3).
    */
-  public street: string[];
+  street: string[];
 
   /**
    * The name of the sender or recipient at the address, if applicable.
    *
    * This field may be empty.
    */
-  public name: string;
+  name: string;
 
   /**
    * The company name, if this is a known business address.
    *
    * This field may be empty.
    */
-  public company: string;
+  company: string;
 
   /**
    * The phone number associated with this address, if any.
    *
    * This field may be empty.
    */
-  public phone: string;
+  phone: string;
 
   /**
    * The city or locality.
    */
-  public cityLocality: string;
+  cityLocality: string;
 
   /**
    * The state or province.
    */
-  public stateProvince: string;
+  stateProvince: string;
 
   /**
    * The postal code.
    */
-  public postalCode: string;
+  postalCode: string;
 
   /**
    * The ISO 3166 country code
    *
    * @see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
    */
-  public country: Country;
+  country: Country;
 
   /**
    * Indicates whether the address is residential or commercial.
    * If unknown, this field will be undefined.
    */
-  public isResidential?: boolean;
-
-  public constructor(address: NormalizedAddressDTO) {
-    this.street = address.street;
-    this.name = address.name || "";
-    this.company = address.company_name || "";
-    this.phone = address.phone || "";
-    this.cityLocality = address.city_locality;
-    this.stateProvince = address.state_province;
-    this.postalCode = address.postal_code;
-    this.country = address.country_code;
-  }
+  isResidential?: boolean;
 
   /**
    * Formats the address for logging or printing.
    */
-  public toString(): string {
-    const address = [];
-    this.company && address.push(this.company);
-    address.push(...this.street);
-    address.push(
-      `${this.cityLocality}, ${this.stateProvince} ${this.postalCode}`
-    );
-    address.push(this.country);
-    return address.join("\n");
-  }
+  toString(): string;
+}
+
+export function normalizeAddress(
+  address: NormalizedAddressDTO
+): NormalizedAddress {
+  return {
+    street: address.street,
+    name: address.name || "",
+    company: address.company_name || "",
+    phone: address.phone || "",
+    cityLocality: address.city_locality,
+    stateProvince: address.state_province,
+    postalCode: address.postal_code,
+    country: address.country_code,
+    isResidential: address.residential || undefined,
+    toString: formatAddress,
+  };
+}
+
+/**
+ * Formats the address for logging or printing.
+ */
+function formatAddress(this: NormalizedAddress): string {
+  const address = [];
+  this.company && address.push(this.company);
+  address.push(...this.street);
+  address.push(
+    `${this.cityLocality}, ${this.stateProvince} ${this.postalCode}`
+  );
+  address.push(this.country);
+  return address.join("\n");
 }
