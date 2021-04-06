955515af615ca92e1dfaa1045800ca8a8cd95318
devmandy <mandy.hubbard@shipengine.com>
2021-04-06 10:00:25 -0500

    Fixes from pair programming including some linting fixes

--- test/specs/client/client.spec.ts
+++ test/specs/client/client.spec.ts
@@ -1,24 +1,24 @@
-import { expect } from 'chai';
-import constants from '../../utils/constants';
-import { InternalRpcClient } from '../../../src/shared/models/client/client';
+import { expect } from "chai";
+import constants from "../../utils/constants";
+import { InternalRpcClient } from "../../../src/shared/models/client/client";
 
-describe('RPC Client test', () => {
-  it('should return correct response if validation error!', async () => {
+describe("RPC Client test", () => {
+  it("should return correct response if validation error!", async () => {
     const client = new InternalRpcClient(
-      'MY_API_KEY',
+      "MY_API_KEY",
       constants.isomorphicBaseUri
     );
-    const response = await client.exec('client', {
-      foo: 'invalid-request',
+    const response = await client.exec("client", {
+      foo: "invalid-request",
     });
 
     response.onError((r) => {
-      expect(r.message).to.eq('Invalid Request');
+      expect(r.message).to.eq("Invalid Request");
       expect(r.code).to.eq(-32600);
-      expect(r).not.to.have.property('id');
+      expect(r).not.to.have.property("id");
     });
     response.onSuccess(() => {
-      expect.fail('should fail.');
+      expect.fail("should fail.");
     });
   });
 });
--- test/utils/constants.js
+++ test/utils/constants.js
@@ -1,10 +1,10 @@
 /**
  * proxy from karma browser to the karma hoverfly server
  */
-const karmaProxyBaseUri = '/hoverfly-proxy';
+const karmaProxyBaseUri = "/hoverfly-proxy";
 const apiBaseUri = `https://simengine.herokuapp.com`;
 
-const onServer = typeof window === 'undefined';
+const onServer = typeof window === "undefined";
 
 const isomorphicBaseUri = onServer ? apiBaseUri : karmaProxyBaseUri;
 
