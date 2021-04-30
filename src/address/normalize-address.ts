b871bb2d53f3563a551b5383e421561dc3504b56
devmandy <mandy.hubbard@shipengine.com>
2021-04-30 14:58:22 -0500

    Updated comments, changed error handling to user ternary

--- src/address/normalize-address.ts
+++ src/address/normalize-address.ts
@@ -18,7 +18,7 @@ export async function normalizeAddress(
   config: NormalizedConfig,
   events: EventEmitter
 ): Promise<NormalizedAddress> {
-  // Calls the validateAddress method and uses its output
+  // Calls the validateAddress method since it uses the same RPC API call
   const result: AddressValidationResult = await validateAddress(
     address,
     config,
@@ -29,14 +29,19 @@ export async function normalizeAddress(
     throw new ShipEngineError(
       ErrorType.BusinessRules,
       ErrorCode.InvalidAddress,
-      "Invalid address." + "\n" + result.errors.join(`\n`)
+      "Invalid address." +
+        (result.errors.length === 1
+          ? result.errors[0]
+          : "\n" + result.errors.join(`\n`))
     );
   }
 
   return result.normalizedAddress;
 }
 
-// Type Guard. If this function returns true, we can know that the value passed in is of type AddressValidationSuccess
+/**
+ * Type Guard ensures we have an AddressValidationResultSuccess
+ */
 function resultIsSuccessful(
   result: AddressValidationResult
 ): result is AddressValidationResultSuccess {
