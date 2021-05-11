7b5e64ebe005a69229f4e09cf3fa6ec454c7346c
devmandy <mandy.hubbard@shipengine.com>
2021-05-11 15:28:01 -0500

    Added carrier-codes

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,7 +1,7 @@
+export * from "./carrier-code";
 export * from "./country";
 export * from "./error-code";
 export * from "./error-source";
 export * from "./error-type";
 export * from "./validation-message-type";
 export * from "./validation-message-code";
-
