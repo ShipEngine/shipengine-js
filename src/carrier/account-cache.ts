da3ba9506136774f0cdc8542c3e2e79bb1e51fc1
devmandy <mandy.hubbard@shipengine.com>
2021-05-13 15:55:42 -0500

    Added account cache

--- src/carrier/account-cache.ts
+++ src/carrier/account-cache.ts
@@ -1,3 +1,38 @@
 import { CarrierAccount } from "./public-types";
 
-export const carrierCache: CarrierAccount[] = [];
+/**
+ * Constant variable used to hold cached carrier accounts.
+ */
+const accountCache: CarrierAccount[] = [];
+
+/**
+ * This function is called by the getCarrierAccounts method to get the cached carrier accounts, filtering
+ * by carrierId if appropriate.
+ */
+export function getAccountCache(carrierCode?: string): CarrierAccount[] {
+  if (accountCache.length === 0) {
+    return accountCache;
+  }
+  if (carrierCode) {
+    const filteredAccounts: CarrierAccount[] = [];
+
+    for (const account of accountCache) {
+      if (account.carrier.code === carrierCode) {
+        filteredAccounts.push(account);
+      }
+    }
+    return filteredAccounts;
+  }
+  return accountCache;
+}
+
+/**
+ * This function is called by getCarrierAccounts to update the cache anytime an RPC call is made to get carriers.
+ */
+export function setAccountCache(accounts: CarrierAccount[]): void {
+  // Set the length to 0 to clear the cache
+  accountCache.length = 0;
+  for (const account of accounts) {
+    accountCache.push(account);
+  }
+}
