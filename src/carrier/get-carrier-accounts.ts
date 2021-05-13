e724dc6cb6fa1a8041df370d8cc2e9d917574e79
devmandy <mandy.hubbard@shipengine.com>
2021-05-13 16:00:14 -0500

    Refactor to use account cache

--- src/carrier/get-carrier-accounts.ts
+++ src/carrier/get-carrier-accounts.ts
@@ -1,12 +1,11 @@
 import { EventEmitter } from "../isomorphic.node";
 import { NormalizedConfig } from "../config";
 import { callJsonRpcMethod, CarriersDTOResult } from "../json-rpc";
-
-import { CarriersDTO } from "../json-rpc";
 import { CarrierAccount } from "./public-types";
 import { carrierNames } from "./carrier-names";
 import { CarrierCode, ErrorCode, ErrorType } from "../enums";
-import { InvalidFieldValueError, ShipEngineError } from "../errors";
+import { ShipEngineError } from "../errors";
+import { getAccountCache, setAccountCache } from "./account-cache";
 
 const method = "carrier.listAccounts.v1";
 
@@ -26,24 +25,27 @@ export async function getCarrierAccounts(
     );
   }
 
-  const params = {
-    carrierCode,
-  };
+  const params = { carrierCode };
 
-  const result: CarriersDTOResult = await callJsonRpcMethod(
-    method,
-    params,
-    config,
-    events
-  );
+  let result: CarriersDTOResult;
 
-  return formatGetCarriersResults(result);
+  // Check the cache before calling the RPC API.
+  const carrierAccountsCache: CarrierAccount[] = getAccountCache();
+
+  // If cache is empty, fetch accounts
+  if (carrierAccountsCache.length === 0) {
+    result = await callJsonRpcMethod(method, params, config, events);
+    const formattedResults = formatGetCarriersResult(result);
+    // Reset the account cache
+    setAccountCache(formattedResults);
+  }
+  return carrierAccountsCache;
 }
 
 /**
- *
+ * Formats the results from the RPC call to match the SDK type..
  */
-function formatGetCarriersResults(result: CarriersDTOResult): CarrierAccount[] {
+function formatGetCarriersResult(result: CarriersDTOResult): CarrierAccount[] {
   const carriers: CarrierAccount[] = [];
 
   const { carrierAccounts } = result;
@@ -60,9 +62,13 @@ function formatGetCarriersResults(result: CarriersDTOResult): CarrierAccount[] {
     };
     carriers.push(item);
   }
+
   return carriers;
 }
 
+/**
+ * Type Guard to ensure we got a valid carrierCode.
+ */
 function isCarrierCode(code: unknown): code is CarrierCode {
   return Object.values(CarrierCode).includes(code as CarrierCode);
 }
