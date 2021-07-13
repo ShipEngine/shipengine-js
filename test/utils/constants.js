aa3862887db6725e99bf534f56aba78e51a708d7
rkrauskopf <randall.krauskopf@gmail.com>
2021-07-13 15:50:16 -0500

    W.I.P.

--- test/utils/constants.js
+++ test/utils/constants.js
@@ -6,8 +6,7 @@
  */
 exports.baseURL =
   process.env["BASE_URL"] ||
-  // "https://shipengine-web-api.herokuapp.com/jsonrpc/";
-  "http://localhost:4000/jsonrpc/";
+  "https://shipengine-web-api.herokuapp.com/jsonrpc/";
 
 /**
  * The API key to use for testing. If not set, it defaults to a dummy key.
