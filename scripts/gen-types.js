79397fb2a264cedd79680f08c9f39d9645f293bc
rkrauskopf <randall.krauskopf@gmail.com>
2021-07-23 14:08:32 -0500

    W.I.P.

--- scripts/gen-types.js
+++ scripts/gen-types.js
@@ -37,3 +37,8 @@ compileFromFile('node_modules/shipengine-json-schema/responses/list_carriers_res
 // Track By Label ID
 // compileFromFile('node_modules/shipengine-json-schema/responses/get_tracking_log_from_label_response_body.json')
 // .then(ts => fs.writeFileSync('src/track-by-label-id/types/private-response.ts', ts))
+
+
+// Track By Carrier Code and Tracking Number
+compileFromFile('node_modules/shipengine-json-schema/responses/get_tracking_log_response_body.json')
+.then(ts => fs.writeFileSync('src/track-by-tracking-number/types/private-response.ts', ts))
--- src/track-by-tracking-number/index.ts
+++ src/track-by-tracking-number/index.ts
@@ -1,1 +1,27 @@
-export {};
+import { NormalizedConfig } from "../config";
+import { get } from "../client";
+import * as TrackPackageByLabelIdTypes from "./types/public";
+import { Response } from "./types/private";
+import { formatResponse } from "./format-response";
+import { validateParams } from "./validate-params";
+
+export * as TrackPackageByLabelIdTypes from "./types/public";
+
+/**
+ * Returns the tracking information of a package identified by its label id.
+ *
+ * @see https://www.shipengine.com/docs/tracking/track-by-label-id
+ */
+export async function trackPackage(
+  params: TrackPackageByLabelIdTypes.Params,
+  config: NormalizedConfig
+): Promise<TrackPackageByLabelIdTypes.Response> {
+  validateParams(params);
+
+  const response = await get<Response.GetTrackingLogFromLabelResponseBody>(
+    `/v1/labels/${params.labelId}/track`,
+    config
+  );
+
+  return formatResponse(response);
+}
