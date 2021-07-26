e86a3e33b99dabb2401fa6468dbdba8026b3ffcf
Pierce Harmon <pierceh.dev@gmail.com>
2021-07-26 13:34:20 -0500

    remove empty string responses from validate address

--- src/validate-addresses/format-response.ts
+++ src/validate-addresses/format-response.ts
@@ -17,15 +17,15 @@ function formatAddressValidationResult(
   return {
     status: result.status,
     originalAddress: {
-      name: originalAddressResponse.name || "",
-      companyName: originalAddressResponse.company_name || "",
-      addressLine1: originalAddressResponse.address_line1 || "",
-      addressLine2: originalAddressResponse.address_line2 || "",
-      addressLine3: originalAddressResponse.address_line3 || "",
-      cityLocality: originalAddressResponse.city_locality || "",
-      stateProvince: originalAddressResponse.state_province || "",
-      postalCode: originalAddressResponse.postal_code || "",
-      countryCode: (originalAddressResponse.country_code as Country) || "",
+      name: originalAddressResponse.name,
+      companyName: originalAddressResponse.company_name,
+      addressLine1: originalAddressResponse.address_line1,
+      addressLine2: originalAddressResponse.address_line2,
+      addressLine3: originalAddressResponse.address_line3,
+      cityLocality: originalAddressResponse.city_locality,
+      stateProvince: originalAddressResponse.state_province,
+      postalCode: originalAddressResponse.postal_code,
+      countryCode: originalAddressResponse.country_code as Country,
       addressResidentialIndicator:
         originalAddressResponse.address_residential_indicator,
     },
@@ -33,8 +33,8 @@ function formatAddressValidationResult(
       normalizedAddressResponse &&
       mapNormalizedAddress(normalizedAddressResponse),
     messages: result.messages.map((m) => ({
-      detailCode: m.detail_code! || "",
-      message: m.message || "",
+      detailCode: m.detail_code!,
+      message: m.message,
       type: mapMessageType(m.type!),
     })),
   };
@@ -57,15 +57,15 @@ function mapNormalizedAddress(
   normalizedAddressResponse: Response.PartialAddress
 ): ValidateAddressesTypes.Address {
   return {
-    name: normalizedAddressResponse.name || "",
-    companyName: normalizedAddressResponse.company_name || "",
-    addressLine1: normalizedAddressResponse.address_line1 || "",
-    addressLine2: normalizedAddressResponse.address_line2 || "",
-    addressLine3: normalizedAddressResponse.address_line3 || "",
-    cityLocality: normalizedAddressResponse.city_locality || "",
-    stateProvince: normalizedAddressResponse.state_province || "",
-    postalCode: normalizedAddressResponse.postal_code || "",
-    countryCode: (normalizedAddressResponse.country_code as Country) || "",
+    name: normalizedAddressResponse.name,
+    companyName: normalizedAddressResponse.company_name,
+    addressLine1: normalizedAddressResponse.address_line1,
+    addressLine2: normalizedAddressResponse.address_line2,
+    addressLine3: normalizedAddressResponse.address_line3,
+    cityLocality: normalizedAddressResponse.city_locality,
+    stateProvince: normalizedAddressResponse.state_province,
+    postalCode: normalizedAddressResponse.postal_code,
+    countryCode: normalizedAddressResponse.country_code as Country,
     addressResidentialIndicator:
       normalizedAddressResponse.address_residential_indicator,
   };
