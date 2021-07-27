6e0506ed95e089f9f0f5c14ba0ffcb773c8e67e7
John Johnson <jrjohnson92@gmail.com>
2021-07-27 11:24:15 -0500

    comments

--- src/create-label/format-response.ts
+++ src/create-label/format-response.ts
@@ -6,35 +6,35 @@ export function formatResponse(
   params: Response.CreateLabelResponseBody
 ): CreateLabelTypes.Response {
   return {
-    labelId: params.label_id!,
-    status: params.status!,
-    shipmentId: params.shipment_id!,
-    shipDate: params.ship_date!,
-    createdAt: params.created_at!,
-    shipmentCost: params.shipment_cost!,
-    insuranceCost: params.insurance_cost!,
-    trackingNumber: params.tracking_number!,
-    isReturnLabel: params.is_return_label!,
-    rmaNumber: params.rma_number!,
-    isInternational: params.is_international!,
-    batchId: params.batch_id!,
-    carrierId: params.carrier_id!,
-    chargeEvent: params.charge_event!,
-    serviceCode: params.service_code!,
-    packageCode: params.package_code!,
-    voided: params.voided!,
-    voidedAt: params.voided_at!,
-    labelFormat: params.label_format!,
-    displayScheme: params.display_scheme!,
-    labelLayout: params.label_layout!,
-    trackable: params.trackable!,
-    labelImageId: params.label_image_id!,
-    carrierCode: params.carrier_code!,
-    trackingStatus: params.tracking_status!,
-    labelDownload: mapLabelDownload(params.label_download!),
-    formDownload: mapFormDownload(params.form_download),
-    insuranceClaim: mapInsuranceClaim(params.insurance_claim),
-    packages: mapPackages(params.packages!)!,
+    labelId: params.label_id!, // Error in generated types
+    status: params.status!, // Error in generated types
+    shipmentId: params.shipment_id!, // Error in generated types
+    shipDate: params.ship_date!, // Error in generated types
+    createdAt: params.created_at!, // Error in generated types
+    shipmentCost: params.shipment_cost!, // Error in generated types
+    insuranceCost: params.insurance_cost!, // Error in generated types
+    trackingNumber: params.tracking_number!, // Error in generated types
+    isReturnLabel: params.is_return_label!, // Error in generated types
+    rmaNumber: params.rma_number!, // Error in generated types
+    isInternational: params.is_international!, // Error in generated types
+    batchId: params.batch_id!, // Error in generated types
+    carrierId: params.carrier_id!, // Error in generated types
+    chargeEvent: params.charge_event!, // Error in generated types
+    serviceCode: params.service_code!, // Error in generated types
+    packageCode: params.package_code!, // Error in generated types
+    voided: params.voided!, // Error in generated types
+    voidedAt: params.voided_at!, // Error in generated types
+    labelFormat: params.label_format!, // Error in generated types
+    displayScheme: params.display_scheme!, // Error in generated types
+    labelLayout: params.label_layout!, // Error in generated types
+    trackable: params.trackable!, // Error in generated types
+    labelImageId: params.label_image_id!, // Error in generated types
+    carrierCode: params.carrier_code!, // Error in generated types
+    trackingStatus: params.tracking_status!, // Error in generated types
+    labelDownload: mapLabelDownload(params.label_download!), // Error in generated types
+    formDownload: mapFormDownload(params.form_download), // Error in generated types
+    insuranceClaim: mapInsuranceClaim(params.insurance_claim), // Error in generated types
+    packages: mapPackages(params.packages!)!, // Error in generated types
   };
 }
 
@@ -42,10 +42,10 @@ function mapLabelDownload(
   params: Response.LabelDownload
 ): CreateLabelTypes.Response["labelDownload"] {
   return {
-    href: params.href!,
-    pdf: params.pdf!,
-    png: params.png!,
-    zpl: params.zpl!,
+    href: params.href!, // Error in generated types
+    pdf: params.pdf!, // Error in generated types
+    png: params.png!, // Error in generated types
+    zpl: params.zpl!, // Error in generated types
   };
 }
 
@@ -54,8 +54,8 @@ function mapFormDownload(
 ): CreateLabelTypes.Response["formDownload"] | null {
   if (!params) return null;
   return {
-    href: params.href!,
-    type: params.type!,
+    href: params.href!, // Error in generated types
+    type: params.type!, // Error in generated types
   };
 }
 
@@ -64,8 +64,8 @@ function mapInsuranceClaim(
 ): CreateLabelTypes.Response["insuranceClaim"] | null {
   if (!params) return null;
   return {
-    href: params.href!,
-    type: params.type!,
+    href: params.href!, // Error in generated types
+    type: params.type!, // Error in generated types
   };
 }
 
@@ -73,12 +73,12 @@ function mapPackages(
   params: Response.Package[]
 ): CreateLabelTypes.Response["packages"][0][] {
   return params.map((pkg) => ({
-    packageCode: pkg.package_code!,
-    trackingNumber: pkg.tracking_number!,
-    weight: pkg.weight!,
-    dimensions: pkg.dimensions!,
-    insuredValue: pkg.insured_value!,
-    labelMessages: pkg.label_messages!,
-    externalPackageId: pkg.external_package_id!,
+    packageCode: pkg.package_code!, // Error in generated types
+    trackingNumber: pkg.tracking_number!, // Error in generated types
+    weight: pkg.weight!, // Error in generated types
+    dimensions: pkg.dimensions!, // Error in generated types
+    insuredValue: pkg.insured_value!, // Error in generated types
+    labelMessages: pkg.label_messages!, // Error in generated types
+    externalPackageId: pkg.external_package_id!, // Error in generated types
   }));
 }
